70b6693b5a16954e25454fd6d8ff6f5a16eb70ce
asm1127 <marjona1marjona2@gmail.com>
2025-10-03 18:12:36 +0500

    S.Marjona masalalari yechimi

---
+++ operator masalalari.py
@@ -0,0 +1,123 @@
+# #1
+# a = int(input("Son1 kiriting: "))
+# b = int(input("Son2 kiriting: "))
+# sum = a + b
+# print(f"Yig'indi: {sum}")
+
+# #2
+# uzunlik = int(input("To'rtburchakning yuzini kiriting: "))
+# eni = int(input("Uchburchakning enini kiriting: "))
+# area = uzunlik * eni
+# print(f"Yuza: {area}")
+
+# #3
+# x = int(input("Son1 kiriting: "))
+# y = int(input("Son2 kiriting: "))
+# z = int(input("Son3 kiriting: "))
+# avg = (x + y + z) / 3
+# print(f"O'rtacha arifmetiki: {avg}")
+
+# #4
+# a = int(input("To'rtburchakning tomonini kiriting: "))
+# b = int(input("To'rtburchakning tomonini kiriting: "))
+# p = 2 * (a + b)
+# print(f"Perimetr: {p}")
+
+# #5
+# h = int(input("Soatni kiriting: "))
+# m = int(input("Daqiqani kiriting: "))
+# total_min = h * 60 + m
+# print(f"Umumiy daqiqalar: {total_min}")
+
+# #6
+# usd = int(input("Dollar miqdori: "))
+# summa = usd * 1200
+# print(f"So'mda: {summa}")
+
+# #7
+# a = int(input("Son1 kiriting: "))
+# b = int(input("Son2 kiriting: "))
+# diff = abs(a - b)
+# print(f"Moduli: {diff}")
+
+# #8
+# x = int(input("Asosni kiriting: "))
+# n = int(input("Darajami kiriting: "))
+# power = x ** n
+# print(f"Hisob: {power}")
+
+# #9
+# a = int(input("Son1 kiriting: "))
+# b = int(input("Son2 kiriting: "))
+# quotient = a // b
+# print(f"Hisob: {quotient}")
+
+# #10
+# a = int(input("Son1 kiriting: "))
+# b = int(input("Son2 kiriting: "))
+# r = a, b = b, a
+# print(f"Almashtirildi: {r}")
+
+#                                   Hayotiy muammolar
+
+# 1
+# total_rent = int(input("Uy ijarasi: "))
+# n = int(input("To'lov: "))
+# per_month = total_rent / n
+# print(f"Har oy {per_month} to'lanadi")
+
+# #2
+# d = int(input("Masofa (km): "))
+# v = int(input("O'rtacha tezlik (km/h): "))
+# time = d / v
+# print(f"Vaqt: {time}")
+
+# #3
+# seats = int(input("Avtobusdagi o'rinlar: "))
+# pessemgers = int(input("Yo'lovchilar: "))
+# per_person = pessemgers // seats
+# left = pessemgers % seats
+# print(f"Chiptalar: {per_person}, Qolgan: {left}")
+
+# #4
+# consumption = int(input("Ishlatilgan yonilg'i: "))
+# d = int(input("Bosilgan masofa: "))
+# fuel = consumption * d / 100
+# print(f"Sarf qilingan yonilg'i: {fuel}")
+
+# #5
+# salary = int(input("Maosh: "))
+# net = salary * (1-0.12)
+# print(f"Netto maosh: {net}")
+
+#6
+# amout = int(input("Summani kiriting: "))
+# days = int(input("Kechikilgan kunni kiriting: "))
+# penalty = amout * 0.001 * days
+# print(f"Jami to'lov: {penalty}")
+
+# #7
+# pice = int(input("Mahsulot narxini kiriting: "))
+# discount = int(input("Chegirma: "))
+# paid = pice * (1 - discount / 100)
+# print(f"To'lanadigan summa: {paid}")
+
+# #8
+# P = int(input("Asos: "))
+# r = int(input("Foiz stavkasi: "))
+# t = int(input("Yillar: "))
+# tinterest = P * r / 100 * t
+# print(f"Jami summa: {tinterest}")
+
+# #9
+# base_qyt = int(input("Ingredient: "))
+# people = int(input("Kerak bo'lgan insonlar: "))
+# per_person = base_qyt / 4
+# needed = per_person * people
+# print(f"{people} kishi uchun {needed} gramm kerak")
+
+# #10
+# bill = int(input("Hisob-kitob: "))
+# tip = bill * 0.10
+# total = bill + tip
+# print(f"Jami summa: {total}")
